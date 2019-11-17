a61d337f1917180789fb9ddb796c2f867ce70c1c
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-11-17 17:05:52 +0530

    new

--- java_coading_challenege/src/java_coading_challenege/hash.java
+++ java_coading_challenege/src/java_coading_challenege/hash.java
@@ -1,6 +1,26 @@
 import java.util.*;
 class hash{
-    public static void main(String [args]){
-        
+    public static void main(String args[]){
+
+        //hash tabels are part of java.util
+        // there are concrete wat to create dictionary
+        // hash tabels are reengineered to add map interface 
+        // Hashtable()
+         // Hashtable(int n)
+         // hashtable(int n ,float s)
+
+         Hashtable balance=new Hashtable();
+         Enumeration <String> names;
+         String str;
+         double bal;
+         balance.put("gokul",1000.10);
+         System.out.println(balance.get("gokul"));
+         balance.put("apple","green");
+         System.out.println(balance.get("gokul"));
+
+
+
+
+
     }
 }
---
+++ java_coading_challenege/src/java_coading_challenege/link.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class link{
+    public static void main(String []args){
+    
+    LinkedList <String> l1=new LinkedList <String> ();
+    l1.add("f");
+    l1.add("qs");
+    l1.add("f");
+    l1.add("smsf");
+    l1.add("f");
+    l1.add("f");
+    l1.add("f");
+    l1.add("f");
+    l1.add("f");
+    l1.addFirst("m");
+    l1.addLast("d");
+    System.out.println(l1);
+    l1.remove("f");
+    System.out.println(l1);
+
+    
+
+    }
+}
