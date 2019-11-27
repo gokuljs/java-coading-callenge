d602c2c38c1d55824e00e8dd8aa69d74d22a26cd
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-11-27 21:49:16 +0530

    28/11/2019

--- java_coading_challenege/src/java_coading_challenege/secondhigest.java
+++ java_coading_challenege/src/java_coading_challenege/secondhigest.java
@@ -10,13 +10,29 @@ class secondhigest{
         for (int i=0;i<n;i++){
             a[i]=sc.nextInt();
         }
-        int max=a[0];
-        for(int i=0;i<n;i++){
-            if(a[i]>max){
-                max=a[i];
+        int temp;
+        System.out.println("sorting element in descending order");
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                if(a[i]<a[j]){
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+
+                }
             }
         }
-        System.out.println("max = "+max);
+        System.out.println("sorted array");
+        for (int i=0;i<n;i++){
+            System.out.println(a[i]);
+
+        }
+        int max1,max2=a[0];
+        
+
+
+        
+
 
 
 
