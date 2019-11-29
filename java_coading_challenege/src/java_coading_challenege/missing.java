cb706846d9fb0cd8af2a14893db363febf830357
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-11-30 01:46:10 +0530

    missing numbers

--- java_coading_challenege/src/java_coading_challenege/missing.java
+++ java_coading_challenege/src/java_coading_challenege/missing.java
@@ -1,18 +1,22 @@
 package java_coading_challenege;
 import java.util.Scanner;
 import java.util.Random;
-import java.util.hashMap;
-class duplicate{
-    public static void main(String []args){
-        //program to remove dupliactes from an array 
-
-        Scanner sc=new Scanner(System.in);
+class missing{
+     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
         System.out.println("enter the array size");
         int n=sc.nextInt();
+        int total = (n + 1) * (n + 2) / 2; 
+
         int []a=new int [100];
         for (int i=0;i<n;i++){
             a[i]=sc.nextInt();
         }
+        for (int i=0;i<n;i++){
+            total=total-a[i];
+
+        }
+        System.out.println("d"+total);
 
     }
 }
