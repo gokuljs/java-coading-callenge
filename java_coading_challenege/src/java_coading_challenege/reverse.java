c4364a3403d16653cb28aa0c79d678013791ed65
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-11-29 17:04:58 +0530

    orign master

--- java_coading_challenege/src/java_coading_challenege/reverse.java
+++ java_coading_challenege/src/java_coading_challenege/reverse.java
@@ -6,6 +6,12 @@ class reverse {
     {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter the size of an array");
+        int n=sc.nextInt();
+        int []a=new int [100];
+        for (int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+
 
     }
 }
