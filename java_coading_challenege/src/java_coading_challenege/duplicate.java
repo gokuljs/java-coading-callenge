4c38311c96e2d4b472a25306efdc880fac9e1b0d
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-11-30 00:25:28 +0530

    finding dupliactes

---
+++ java_coading_challenege/src/java_coading_challenege/duplicate.java
@@ -0,0 +1,17 @@
+package java_coading_challenege;
+import java.util.Scanner;
+import java.util.Random;
+class duplicate{
+    public static void main(String []args){
+        //program to remove dupliactes from an array 
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the array size");
+        int n=sc.nextInt();
+        int []a=new int [100];
+        for (int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+
+    }
+}
