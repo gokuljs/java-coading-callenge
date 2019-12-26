1cd7c75441b195bcff679206b11145c544654760
gokul js <51263906+gokuljs@users.noreply.github.com>
2019-12-26 23:08:04 +0530

    kaknxka

--- code2.0/pali.java
+++ code2.0/pali.java
@@ -12,21 +12,27 @@ class pali{
         System.out.println(f[i]);
         }
         char[] z=new char[100];
-        for(int j=l.length()-1;j>-1;j--){
-            z[j]=f[j];
-        }
+        
         System.out.println(" reversed array ");
-        for(int j=l.length()-1;j>-1;j--){
-            System.out.println(z[j]);
+        int j=l.length();
+        for (int i=0;i<l.length();i++){
+            z[j-1]=f[i];
+            j--;
+        }
+        for (int i=0;i<l.length();i++){
+            System.out.println(z[i]);
         }
         int flag=0;
-        for(int i=0;i<l.length();i++){
-            for (int j=l.length()-1;j>-1;j--){
-                if (f[i]==z[j]){
-                    flag=1;
-                }
+        for (int i=0;i<l.length();i++){
+            if(z[i]==f[i]){
+                flag=1;
+
             }
+
         }
+
+
+        
         if (flag==1){
             System.out.println("palindrome");
         }
