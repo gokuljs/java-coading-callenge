79c694b0feaf07280db7c18751d5dea346068a25
gokul js <51263906+gokuljs@users.noreply.github.com>
2020-01-03 20:28:17 +0530

    rev

--- code2.0/dup.java
+++ code2.0/dup.java
@@ -1,2 +1,24 @@
 import java.util.*;
-class 
+class dup{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter ur String");
+        String sp=sc.nextLine();
+        char []ch=sp.toCharArray();
+        int flag =0;
+        for(int i=0;i<sp.length();i++){
+            for (int j=i+1;j<sp.length();j++){
+                if (ch[i]==ch[j]){
+                    flag=1;
+                    System.out.println(ch[i]);
+
+                }
+                else{
+                    flag=0;
+                }
+
+            }
+        }
+        
+    }
+}
