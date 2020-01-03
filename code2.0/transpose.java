f1ec79fd169ac8e53813406352a86b415d8e4135
gokul js <51263906+gokuljs@users.noreply.github.com>
2020-01-03 19:27:14 +0530

    revise

--- code2.0/transpose.java
+++ code2.0/transpose.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 class transpose{
-    public static void main (String []args){
+    public static void main(String []args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the size of the matrix ");
+        System.out.println("enter the size of the matrix");
         int n=sc.nextInt();
         int [][]a=new int [100][100];
         for(int i=0;i<n;i++){
@@ -10,21 +10,24 @@ class transpose{
                 a[i][j]=sc.nextInt();
             }
         }
-        System.out.println("matrix is ");
-        System.out.println("original matrix is ");
-
+        System.out.println("enter the size of the matrix");
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for (int j=0;j<n;j++){
                 System.out.print(a[i][j]);
             }
-            System.out.println("");
+            System.out.println();
+
         }
         System.out.println("transpose of a matrix");
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for (int j=0;j<n;j++){
                 System.out.print(a[j][i]);
             }
-            System.out.println("");
+            System.out.println();
+
         }
+
+
+
     }
 }
