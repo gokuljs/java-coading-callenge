3b9369f5d5108d09c8c36b0bbd0f7418bacf8bfe
gokuljs <526473+gb96@users.noreply.github.com>
2020-01-12 14:14:46 +0530

    test

--- shift.java
+++ shift.java
@@ -1,27 +1,25 @@
 import java.util.Scanner;
 class shift{
-    public static void main (String []args){
-        int a[]={1,0,0,1,1,0,0,0};
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);  
+        int a[]={1,0,0,0,1,1,1,0};//0,1,2,3,4,5,6,7
         int n=a.length;
-        int count=0;
-        for (int i=0;i<n;i++){
-            if(a[i]!=0){
-                count ++;
+        int j=n-1;
+        int k=0;
+        int []b=new int [n];
+        for(int i=0;i<n;i++){
+            if (a[i]==0){
+                b[k]=0;
+                k++;
+            }
+            if(a[i]==1){
+                b[j]=1;
+                j--;
             }
 
         }
-        System.out.println(count);
         for(int i=0;i<n;i++){
-            if (a[i]!=0){
-                a[count]=0;
-                count ++;
-            }
+            System.out.println(b[i]);
         }
-        for(int i=0;i<n;i++){
-            System.out.println(count);
-        }
-
-        
-
     }
 }
