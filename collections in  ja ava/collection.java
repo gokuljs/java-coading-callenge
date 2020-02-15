978469a6693b1d0839d800dfdd0fa51767eea260
Gokul js <51263906+gokuljs@users.noreply.github.com>
2020-02-15 17:34:03 +0530

    arraylist

--- collections in  ja ava/collection.java
+++ collections in  ja ava/collection.java
@@ -31,12 +31,49 @@ public class collection {
 		System.out.println(list.size());
 		System.out.println(list.get(10));
 		
-		System.out.println(list.trimToSize());
+		// looping through all elements using list iterator
+		// iterator can traverse only in one direction 
+	
+		Iterator li=list.iterator();
+		while(li.hasNext()) {
+			System.out.println(li.next());
+		}
+		
+		li.remove();
+		while(li.hasNext()) {
+			System.out.println(li.next());
+		}
+		
+		// traversing all the elements using for each loop
+		System.out.println("------------------------------------------------------------------");
+
+		for(String obj:list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("------------------------------------------------------------------dd");
+		
+		// traversing all elements using listiterator
+		
+		ListIterator<String> lo=list.listIterator(list.size());
+		while(lo.hasPrevious()) {
+            String str=lo.previous();  
+			System.out.println(str);
+		}
+		System.out.println("------------------------------------------------------------------dd");
+        // traversing all elements using all list using for loop
+		
+		for(int i=0;i<list.size();i++) {
+			String s=list.get(i);
+			System.out.println(s);
+			
+		}
+		
+		
+		
 
 
 
-		list.clear();
-		System.out.println(list);
 		
 
 
